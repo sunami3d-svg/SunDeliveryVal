f789db5fae2b68db4c9d10d5bfe15d9f5c11c1e2
SunLogistica <sunami3d@gmail.com>
2026-08-14 17:24:37 -0400

    Add files via upload
    Refactor: separación de archivos JS y Firebase

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+            apiKey: "AIzaSyA147jH0Tt_ZlJyAmoaeJKdDMtNF1VG4E4",
+            authDomain: "sunvalencia-609f5.firebaseapp.com",
+            projectId: "sunvalencia-609f5",
+            storageBucket: "sunvalencia-609f5.firebasestorage.app",
+            messagingSenderId: "339030334268",
+            appId: "1:339030334268:web:3be1db9248948343591c39",
+            measurementId: "G-N345DPZFFG"
+        };
+
+        firebase.initializeApp(firebaseConfig);
+        const db = firebase.firestore();
